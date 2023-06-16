ab7afb08772d9325bcac7e9adec504dc54a44e7b
Schaeffer2013 <r-sschaeffer@live.com>
2023-06-16 15:23:55 -0400

    incommit

---
+++ app.py
@@ -0,0 +1,24 @@
+import copy 
+import constants
+from constants import PLAYERS, TEAMS
+TEAMS = []
+PLAYERS = {}
+
+def clean_players():
+    global TEAMS
+    global PLAYERS
+
+    cleaned = []
+    for players in constants:
+        fixed = {}
+        fixed ["name"] = players["name"]
+        fixed ["guardians"] = players["guardians"].split(" and ")[0]
+        if players["experience"] == "True":
+            fixed["experience"] = True
+        else:
+            fixed["experience"] = False
+        fixed["height"] = int(players["height"])
+        cleaned.append(fixed)
+    return cleaned
+
+    def balance_teams():
